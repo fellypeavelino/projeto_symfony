6399905b26c88108d9fe79454f39de81da435e5a
pedro felipe <fellypeavelino@hotmail.com>
2016-09-11 07:15:02 -0300

    mais registro na base

--- symfony.sql
+++ symfony.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Tempo de geração: 11/09/2016 às 07:07
+-- Tempo de geração: 11/09/2016 às 07:14
 -- Versão do servidor: 10.0.25-MariaDB-0ubuntu0.16.04.1
 -- Versão do PHP: 7.0.8-3+deb.sury.org~trusty+1
 
@@ -44,7 +44,11 @@ INSERT INTO `chamados` (`id`, `cliente_id`, `pedidos_id`, `titulo`, `observacao`
 (3, 3, 6, 'teste4', 'teste4'),
 (4, 4, 6, 'teste5', 'teste5'),
 (5, 5, 5, 'teste6', 'teste6'),
-(6, 6, 6, 'teste7', 'teste7');
+(6, 6, 6, 'teste7', 'teste7'),
+(7, 7, 5, 'teste8', 'teste8'),
+(8, 8, 5, 'teste9', 'teste9'),
+(9, 9, 5, 'teste10', 'teste10'),
+(10, 10, 6, 'teste11', 'teste11');
 
 -- --------------------------------------------------------
 
@@ -68,7 +72,11 @@ INSERT INTO `cliente` (`id`, `nome`, `email`) VALUES
 (3, 'teste4', 'incubus@hotmail.com'),
 (4, 'teste5', 'teste5@hotmail.com'),
 (5, 'teste6', 'teste6@gmail.com'),
-(6, 'teste7', 'teste7@gmail.com');
+(6, 'teste7', 'teste7@gmail.com'),
+(7, 'maria', 'felipeavel@hotmail.com'),
+(8, 'avelino', 'teste9@gmail.com'),
+(9, 'teste10', 'teste10@gmail.com'),
+(10, 'teste11', 'teste11@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -126,12 +134,12 @@ ALTER TABLE `pedidos`
 -- AUTO_INCREMENT de tabela `chamados`
 --
 ALTER TABLE `chamados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT de tabela `pedidos`
 --
